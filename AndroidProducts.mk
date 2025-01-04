d6ee031467f4cc2ab201aec968d9e2877a1cf76d
angelomds42 <angelomds42@gmail.com>
2025-01-04 16:16:03 -0300

    spes: Init @PixelOS-AOSP
    Change-Id: I97c84127f103c06a28899b95546fe4a288b4f5b6
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_spes.mk
+    $(LOCAL_DIR)/custom_spes.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_spes-user \
-    lineage_spes-userdebug \
-    lineage_spes-eng
+    custom_spes-user \
+    custom_spes-userdebug \
+    custom_spes-eng
