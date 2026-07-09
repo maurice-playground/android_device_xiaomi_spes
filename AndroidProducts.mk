a5ee150b74fcb4610697ae2fd4e0b4f1e8e69f7c
spsesmynuts <rianemaster1221@gmail.com>
2026-07-09 09:35:10 +0800

    spes: Init @PixelOS-AOSP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_spes.mk
+    $(LOCAL_DIR)/custom_spes.mk
--- custom_spes.mk
+++ custom_spes.mk
@@ -12,27 +12,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 # Product Specifics
-PRODUCT_NAME := lineage_spes
+PRODUCT_NAME := custom_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Build Flags
-TARGET_ENABLE_BLUR := true
-TARGET_INCLUDE_AXFX := false
-AXION_CAMERA_REAR_INFO := 50,8,2,2
-AXION_CAMERA_FRONT_INFO := 13
-AXION_MAINTAINER := spesmynuts
-AXION_PROCESSOR := Qualcomm_Snapdragon_680_4G
-TARGET_INCLUDES_LOS_PREBUILTS := true
 
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
