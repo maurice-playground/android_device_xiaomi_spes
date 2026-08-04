2a827a7e30e05ee9f326d08d8fc5afbcee33875f
spsesmynuts <rianemaster1221@gmail.com>
2026-08-04 20:21:38 +0800

    spes: Initialize @Lunaris-AOSP

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -16,6 +16,9 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Inherit my keys
+$(call inherit-product, vendor/lineage-priv/keys/keys.mk)
+
 # Product Specifics
 PRODUCT_NAME := lineage_spes
 PRODUCT_DEVICE := spes
@@ -26,14 +29,17 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Build Flags
-TARGET_INCLUDE_GOOGLE_TELECOMM := false
-TARGET_ENABLE_BLUR := false
-TARGET_INCLUDE_AXFX := false
-AXION_CAMERA_REAR_INFO := 50,8,2,2
-AXION_CAMERA_FRONT_INFO := 13
-AXION_MAINTAINER := spesmynuts
-AXION_PROCESSOR := Qualcomm_Snapdragon_680_4G
-TARGET_INCLUDES_LOS_PREBUILTS := true
+TARGET_OPTIMIZED_DEXOPT := false
+BYPASS_CHARGE_SUPPORTED := false
+TARGET_ENABLE_BLUR := true
+TARGET_CUSTOM_UDFPS := false
+USE_REALITY_ENGINE := true
+WITH_PIXEL_LAUNCHER := false
+
+# Customized GApps
+WITH_GMS := true
+TARGET_USE_WALLPAPERS := true
+TARGET_DISABLE_MATLOG := true
 
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
